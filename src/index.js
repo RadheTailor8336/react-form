d0b5fc6cf23b1b160d146d8ac2950ad35258298b
RadheTailor8336 <radhetailor923@gmail.com>
2025-06-08 16:47:28 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // 👈 This line is necessary
+import './index.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
